30a3cba45128cb1299ee63d48f60394ccedeb7b5
Mohamed Abdelrahman <m7md0a.work@gmail.com>
2024-01-02 20:55:11 +0200

    first commit

---
+++ src/app/(app_pages)/page.tsx
@@ -0,0 +1,101 @@
+export default function Home() {
+  return (
+    <main className="space-y-5">
+      <div className="card bg-base-100">
+        <div className="card-body p-5">
+          <div className="flex space-x-5 items-center">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <textarea className="textarea focus:border-none flex-1 placeholder:text-base h-8"  placeholder="What's on your mind?" ></textarea>
+          </div>
+          <div className="flex justify-end space-x-4 [&>span>svg]:w-7 [&>span>svg]:h-7">
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+            <span>
+              <svg className="text-base-content/50" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" data-testid="svg-icon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="card h-40 bg-base-100">
+        <div className="card-body">
+          lkmd
+        </div>
+      </div>
+      <div className="card h-40 bg-base-100">
+        <div className="card-body">
+          lkmd
+        </div>
+      </div>
+      <div className="card h-40 bg-base-100">
+        <div className="card-body">
+          lkmd
+        </div>
+      </div>
+      <div className="card h-40 bg-base-100">
+        <div className="card-body">
+          lkmd
+        </div>
+      </div>
+      <div className="card h-40 bg-base-100">
+        <div className="card-body">
+          lkmd
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/components/layouts/HeaderApp.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function HeaderApp() {
+    return (
+        <div className="navbar container-app">
+            <div className="flex-1 -space-x-0d">
+                <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost drawer-button lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </label>
+                <div className='me-4'>
+                    <a className="btn btn-ghost text-xl -ms-4">Social</a>
+                </div>
+                <div className="form-control d -ms-4 md:-ms-0">
+                    <input type="text" placeholder="Search" className="input input-bordered input-primary input-sm p-4 rounded-box  w-24 md:w-auto" />
+                </div>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
